2298325e5e9322942f43eb047e81b12feb5997e0
Cristian Petra <cristy86_upm@yahoo.com>
2017-07-24 11:57:06 +0300

    Play video file

--- ipopmetrics/VideoScreenViewController.swift
+++ ipopmetrics/VideoScreenViewController.swift
@@ -7,15 +7,55 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class VideoScreenViewController: UIViewController {
+    
+    @IBOutlet weak var containerPlayer: UIView!
+    var player: AVPlayer?
+    var playerController = AVPlayerViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(red: 255/255, green: 221/255, blue: 105/255, alpha: 1.0)
         
+        setupVideoPlayer()
+        
     }
-
-
+    
+    internal func setupVideoPlayer() {
+        self.playerController.contentOverlayView?.backgroundColor = UIColor.red
+        
+        let videoString: String? = Bundle.main.path(forResource: "WelcomeVideo", ofType: ".mov")
+        
+        if let url = videoString {
+            let videoUrl = URL(fileURLWithPath: url)
+            self.player = AVPlayer(url: videoUrl)
+            self.playerController.player = self.player
+        }
+        
+        self.playerController.view.frame = self.containerPlayer.bounds
+        self.playerController.view.frame.origin.x = self.containerPlayer.frame.origin.x
+        self.playerController.view.frame.origin.y = self.containerPlayer.frame.origin.y
+        
+        playerController.showsPlaybackControls = true
+        
+        self.view.addSubview(playerController.view)
+        self.showDetailViewController(self.playerController, sender: self)
+        
+        player?.addObserver(self, forKeyPath: "status", options: NSKeyValueObservingOptions.new, context: nil)
+        player?.addObserver(self, forKeyPath: "rate", options: NSKeyValueObservingOptions.new, context: nil)
+        
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "rate") {
+            self.playerController.view.frame = self.view.bounds
+        }
+        if (keyPath == "status") {
+        }
+    }
+    
 }
