bb66548970d10fe1a6f71b915687b7d26d09f339
Rares Pop <rares.pop@athanasys.com>
2017-09-05 10:44:08 +0300

    a

--- Todo.swift
+++ Todo.swift
@@ -7,7 +7,50 @@
 //
 
 import Foundation
-import UIKit
+import RealmSwift
+
+
+class TodoCard:  Object {
+
+    dynamic var cardId: String? = nil
+    dynamic var index = 0
+    
+    dynamic var type = ""
+    dynamic var section = ""
+    dynamic var headerTitle: String? = nil
+    dynamic var headerSubtitle: String? = nil
+    dynamic var headerIconUri:String? = nil
+    dynamic var message:String? = nil
+    
+    dynamic var actionHandler = ""
+    dynamic var actionLabel = ""
+    dynamic var imageUri:String? = nil
+    dynamic var tooltipTitle: String? = nil
+    dynamic var tooltipContent: String? = nil
+    
+}
+
+class TodoSocialPost: Object {
+    
+    dynamic var todoCard: TodoCard? = nil
+    dynamic var index = 0
+    dynamic var isApproved = false
+    
+    dynamic var type = ""
+    dynamic var status: String? = nil
+    dynamic var statusDate: Date? = nil
+    
+    dynamic var articleCategory:String? = nil
+    dynamic var articleTitle:String? = nil
+    
+    dynamic var articleText = ""
+    dynamic var articleUrl = ""
+    var articleHastags: [Any?] = []
+    dynamic var articleImage:String? = nil
+    
+    
+}
+
 
 class TodoItem: CalendarItem {
     dynamic var isApproved = false
