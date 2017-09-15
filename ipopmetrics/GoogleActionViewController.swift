9621ae7307f989cfea0236e8cf900d28ca0c5e0f
Cristian Petra <cristy86_upm@yahoo.com>
2017-09-15 19:16:38 +0300

    Add view in bottom for social action

--- ipopmetrics/GoogleActionViewController.swift
+++ ipopmetrics/GoogleActionViewController.swift
@@ -13,19 +13,56 @@ class GoogleActionViewController: UIViewController {
     let taskView = TaskGoogleCitationView(frame: CGRect(x: 0, y: 355, width: UIScreen.main.bounds.width, height: 608))
     let recommendedActionView = RecommendedActionGoogleCitationView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 355))
     
-    let containerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.size.height + 1000))
+    let containerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.size.height + 620))
     
     let scrollView: UIScrollView = UIScrollView()
+    
+    let popmetricsInsightView = IndividualTaskView()
+    let recommendedReadingView = IndividualTaskView()
+    let statsView = IndividualTaskView()
+    let aimeeView = IndividualTaskView()
+    let instructionsView = IndividualTaskView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
+        self.view.backgroundColor = UIColor.feedBackgroundColor()
+        
+        setUpNavigationBar()
+        
         addScrollView()
         addContainerView()
         addRecommendedActionView()
         addTaskView()
+        
+        addPopmetricsInsightView()
+        addStatsView()
+        addRecommendedReadingView()
+        addAimeeView()
+        addInstructionView()
+    }
+    
+    func setUpNavigationBar() {
+        
+        let text = UIBarButtonItem(title: "Social Post", style: .plain, target: self, action: nil)
+        text.tintColor = UIColor(red: 67/255, green: 78/255, blue: 84/255, alpha: 1.0)
+        let titleFont = UIFont(name: FontBook.bold, size: 18)
+        text.setTitleTextAttributes([NSFontAttributeName: titleFont], for: .normal)
+        
+        self.navigationController?.navigationBar.backgroundColor = UIColor.white
+        navigationController?.navigationBar.isTranslucent = false
+        
+        let leftButtonItem = UIBarButtonItem.init(image: UIImage(named: "iconCalLeftBold"), style: .plain, target: self, action: #selector(handlerClickBack))
+        //self.navigationItem.leftBarButtonItem = leftButtonItem
+        self.navigationItem.leftBarButtonItems = [leftButtonItem, text]
+        self.navigationItem.leftBarButtonItem?.tintColor = UIColor.black
+        
+    }
+    
+    func handlerClickBack() {
+        self.navigationController?.popViewController(animated: true)
     }
     
     func addScrollView() {
@@ -68,7 +105,138 @@ class GoogleActionViewController: UIViewController {
         taskView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor).isActive = true
         taskView.rightAnchor.constraint(equalTo: self.containerView.rightAnchor).isActive = true
         taskView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 355).isActive = true
-        taskView.heightAnchor.constraint(equalToConstant: 1008).isActive = true
+        taskView.heightAnchor.constraint(equalToConstant: 608).isActive = true
     }
+    
+    func addPopmetricsInsightView() {
+        
+        let insightViewTest = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 200))
+        
+        self.containerView.addSubview(popmetricsInsightView)
+        popmetricsInsightView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor).isActive = true
+        popmetricsInsightView.rightAnchor.constraint(equalTo: self.containerView.rightAnchor).isActive = true
+        popmetricsInsightView.topAnchor.constraint(equalTo: taskView.bottomAnchor,constant: 2).isActive = true
+        popmetricsInsightView.titleLabel.text = "Popmetrics Insight"
+        popmetricsInsightView.titleLabel.textColor = UIColor.white
+        popmetricsInsightView.taskContainerView.addSubview(insightViewTest)
+        popmetricsInsightView.containerView.backgroundColor = UIColor(red: 67/255, green: 76/255, blue: 84/255, alpha: 1)
+        insightViewTest.backgroundColor = UIColor.cyan
+        popmetricsInsightView.taskContainerView.isHidden = true
+        popmetricsInsightView.expandButton.tintColor = UIColor.white
+        insightViewTest.bottomAnchor.constraint(equalTo: popmetricsInsightView.taskContainerView.bottomAnchor).isActive = true
+        popmetricsInsightView.expandButton.parentView = popmetricsInsightView
+        //popmetricsInsightView.expandButton.addTarget(self, action: #selector(showHideContent(sender:)), for: .touchUpInside)
+        //footerView.topAnchor.constraint(equalTo: test.containerStackView.bottomAnchor,constant: 10).isActive = true
+        
+    }
+    
+    func addStatsView() {
+        
+        let insightViewTest = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 200))
+        //let trafficStats = TrafficStatusView(frame: CGRect(x: 0, y: 0, width: 375, height: 500))
+        
+        //thirdTest.taskContainerView.frame = needed view bounds
+        
+        self.containerView.addSubview(statsView)
+        statsView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor).isActive = true
+        statsView.rightAnchor.constraint(equalTo: self.containerView.rightAnchor).isActive = true
+        statsView.topAnchor.constraint(equalTo:popmetricsInsightView.bottomAnchor).isActive = true
+        statsView.titleLabel.text = "Stats"
+        statsView.setUpSubtitleview()
+        statsView.taskContainerView.addSubview(insightViewTest)
+        statsView.containerView.backgroundColor = UIColor.white
+        statsView.backgroundColor = UIColor.belizeHoleColor()
+        statsView.taskContainerView.isHidden = true
+        statsView.subtitleLabel.isHidden = true
+        insightViewTest.bottomAnchor.constraint(equalTo: statsView.taskContainerView.bottomAnchor).isActive = true
+        statsView.expandButton.parentView = statsView
+        //statsView.expandButton.addTarget(self, action: #selector(showHideContent(sender:)), for: .touchUpInside)
+        
+        //footerView.topAnchor.constraint(equalTo: thirdTest.containerStackView.bottomAnchor,constant: 10).isActive = true
+        
+    }
+    
+    func addRecommendedReadingView() {
+        
+        let insightViewTest2 = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 400))
+        
+        self.containerView.addSubview(recommendedReadingView)
+        recommendedReadingView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor).isActive = true
+        recommendedReadingView.rightAnchor.constraint(equalTo: self.containerView.rightAnchor).isActive = true
+        recommendedReadingView.topAnchor.constraint(equalTo:statsView.bottomAnchor).isActive = true
+        recommendedReadingView.titleLabel.text = "Recommended Reading"
+        recommendedReadingView.taskContainerView.addSubview(insightViewTest2)
+        recommendedReadingView.containerView.backgroundColor = PopmetricsColor.darkGrey
+        recommendedReadingView.titleLabel.textColor = UIColor.white
+        recommendedReadingView.expandButton.tintColor = UIColor.white
+        insightViewTest2.backgroundColor = UIColor.belizeHoleColor()
+        recommendedReadingView.taskContainerView.isHidden = true
+        insightViewTest2.bottomAnchor.constraint(equalTo: recommendedReadingView.taskContainerView.bottomAnchor).isActive = true
+        recommendedReadingView.expandButton.parentView = recommendedReadingView
+        //recommendedReadingView.expandButton.addTarget(self, action: #selector(showHideContent(sender:)), for: .touchUpInside)
+    }
+    
+    func addAimeeView() {
+        
+        let insightViewTest = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 200))
+        //let trafficStats = TrafficStatusView(frame: CGRect(x: 0, y: 0, width: 375, height: 500))
+        
+        //thirdTest.taskContainerView.frame = needed view bounds
+        
+        self.containerView.addSubview(aimeeView)
+        aimeeView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor).isActive = true
+        aimeeView.rightAnchor.constraint(equalTo: self.containerView.rightAnchor).isActive = true
+        
+        aimeeView.topAnchor.constraint(equalTo:recommendedReadingView.bottomAnchor).isActive = true // setting top anchor to previous bottom anchor
+        
+        aimeeView.titleLabel.text = "Aimee's View"
+        aimeeView.setUpSubtitleview()
+        aimeeView.taskContainerView.addSubview(insightViewTest)
+        aimeeView.containerView.backgroundColor = UIColor.white
+        aimeeView.backgroundColor = UIColor.belizeHoleColor()
+        aimeeView.taskContainerView.isHidden = true
+        aimeeView.subtitleLabel.isHidden = true
+        insightViewTest.bottomAnchor.constraint(equalTo: aimeeView.taskContainerView.bottomAnchor).isActive = true
+        aimeeView.expandButton.parentView = aimeeView
+        //aimeeView.expandButton.addTarget(self, action: #selector(showHideContent(sender:)), for: .touchUpInside)
+        
+        //this has to be set for the last view cuz the bottom view (footer view) needs to set its top anchor to be able to scroll
+        
+        //footerView.topAnchor.constraint(equalTo: aimeeView.containerStackView.bottomAnchor).isActive = true
+        
+    }
+    
+    func addInstructionView() {
+        
+        let separatorView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 1))
+        separatorView.backgroundColor = UIColor(red: 189/255, green: 197/255, blue: 203/255, alpha: 1)
+        
+        self.containerView.addSubview(instructionsView)
+        
+        instructionsView.containerView.addSubview(separatorView)
+        instructionsView.leftAnchor.constraint(equalTo: self.containerView.leftAnchor).isActive = true
+        instructionsView.rightAnchor.constraint(equalTo: self.containerView.rightAnchor).isActive = true
+        
+        instructionsView.topAnchor.constraint(equalTo:aimeeView.bottomAnchor,constant: 1).isActive = true // setting top anchor to previous bottom anchor
+        
+        instructionsView.titleLabel.text = "View Instructions"
+        instructionsView.setUpSubtitleview()
+        //instructionsView.taskContainerView.addSubview(insightViewTest)
+        instructionsView.containerView.backgroundColor = UIColor.white
+        instructionsView.taskContainerView.isHidden = true
+        instructionsView.titleLabel.font = UIFont(name: FontBook.bold, size: 15)
+        instructionsView.subtitleLabel.isHidden = true
+        //insightViewTest.bottomAnchor.constraint(equalTo: aimeeView.taskContainerView.bottomAnchor).isActive = true
+        //instructionsView.bottomAnchor.constraint(equalTo: backgroundImageView.topAnchor).isActive = true
+        instructionsView.expandButton.setImage(UIImage(named:"iconCalRighttBold"), for: .normal)
+        //aimeeView.expandButton.parentView = aimeeView
+        //aimeeView.expandButton.addTarget(self, action: #selector(showHideContent(sender:)), for: .touchUpInside)
+        
+        //this has to be set for the last view cuz the bottom view (footer view) needs to set its top anchor to be able to scroll
+        
+        //footerView.topAnchor.constraint(equalTo: backgroundImageView.bottomAnchor).isActive = true
+        
+    }
+    
 
 }
---
+++ ipopmetrics/IndividualTaskView.swift
@@ -0,0 +1,166 @@
+//
+//  IndividualTaskView.swift
+//  ipopmetrics
+//
+//  Created by Cristian Petra on 15/09/2017.
+//  Copyright © 2017 Popmetrics. All rights reserved.
+//
+
+import UIKit
+import ActiveLabel
+
+class IndividualTaskView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+        
+    }
+    
+    lazy var containerView: UIView = {
+        
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.lightGray
+        return view
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var subtitleLabel: ActiveLabel = {
+        
+        let label = ActiveLabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var expandButton: ButtonWithContainer = {
+        
+        let button = ButtonWithContainer(type: UIButtonType.system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = UIColor.black//(red: 67, green: 76, blue: 84, alpha: 1)
+        return button
+    }()
+    
+    lazy var tapLayerView: UIView = {
+        
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.white.withAlphaComponent(0)
+        return view
+    }()
+    
+    lazy var taskContainerView: UIView = {
+        
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.yellow
+        return view
+    }()
+    
+    var containerStackView: UIStackView!
+    var isSelected: Bool = false
+    
+    var isContentHidden: Bool = false {
+        didSet{
+            taskContainerView.isHidden = !isContentHidden
+        }
+    }
+    
+    func setup() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        setUpcontainerStackView()
+        setUpContainerView()
+        setUpTaskContainerView()
+        setUpTitleLabel()
+        setUpButton()
+        
+    }
+    
+    private func setUpContainerView() {
+        
+        containerView.leftAnchor.constraint(equalTo: containerStackView.leftAnchor).isActive = true
+        containerView.topAnchor.constraint(equalTo: containerStackView.topAnchor).isActive = true
+        containerView.rightAnchor.constraint(equalTo: containerStackView.rightAnchor).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 66).isActive = true
+        
+    }
+    
+    private func setUpTaskContainerView() {
+        
+        taskContainerView.leftAnchor.constraint(equalTo: containerStackView.leftAnchor).isActive = true
+        taskContainerView.bottomAnchor.constraint(equalTo: containerStackView.bottomAnchor).isActive = true
+        taskContainerView.rightAnchor.constraint(equalTo: containerStackView.rightAnchor).isActive = true
+        taskContainerView.topAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        
+    }
+    
+    func setUpcontainerStackView() {
+        
+        containerStackView = UIStackView(arrangedSubviews: [containerView,taskContainerView])
+        containerStackView.axis = .vertical
+        containerStackView.distribution = .equalSpacing
+        containerStackView.alignment = .center
+        containerStackView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(containerStackView)
+        
+        containerStackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        containerStackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        containerStackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        containerStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+    }
+    
+    func setUpTitleLabel() {
+        
+        containerView.addSubview(titleLabel)
+        titleLabel.textColor = PopmetricsColor.darkGrey
+        titleLabel.font = UIFont(name: FontBook.semibold, size: 18)
+        
+        titleLabel.leftAnchor.constraint(equalTo: containerView.leftAnchor,constant: 20).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20).isActive = true
+        
+    }
+    
+    func setUpButton() {
+        
+        containerView.addSubview(expandButton)
+        expandButton.rightAnchor.constraint(equalTo: containerView.rightAnchor,constant: -20).isActive = true
+        expandButton.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20).isActive = true
+        expandButton.setImage(UIImage(named:"iconDown"), for: .normal)
+        
+    }
+    
+    func setUpSubtitleview() {
+        
+        containerView.addSubview(subtitleLabel)
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 2).isActive = true
+        subtitleLabel.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 20).isActive = true
+        subtitleLabel.textColor = UIColor(red: 155/255, green: 155/255, blue: 155/255, alpha: 1)
+        subtitleLabel.font = UIFont(name: FontBook.regular, size: 12)
+        
+        subtitleLabel.text = "June 23 - July 24     www.hutcheson.io"
+        subtitleLabel.URLColor = UIColor(red: 155/255, green: 155/255, blue: 155/255, alpha: 1)
+        
+        subtitleLabel.configureLinkAttribute = { (type, attribute, isSelected) in
+            
+            var attr = attribute
+            attr[NSFontAttributeName] = UIFont(name: FontBook.bold, size: 12)
+            
+            return attr
+        }
+        
+    }
+    
+}
