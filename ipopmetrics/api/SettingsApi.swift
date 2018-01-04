0eaee11aa8a3817eb079477759b393e204e27919
Cristian Petra <cristy86_upm@yahoo.com>
2018-01-04 12:50:13 +0200

    Settings overlay description wiring
    master/tech#539

---
+++ ipopmetrics/api/SettingsApi.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsApi.swift
+//  ipopmetrics
+//
+//  Created by Cristian Petra on 04/01/2018.
+//  Copyright © 2018 Popmetrics. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class SettingsApi: BaseApi {
+    
+    func changeOverlayDescription(description: String, callback: @escaping (_ error: ApiError?) -> Void) {
+        let url = ""
+        let params = ["description": description]
+        
+        Alamofire.request(url,
+                          method: .post, parameters: params, encoding: JSONEncoding.default,
+                          headers:createHeaders()).responseJSON { response in
+                            if let err = self.createErrorWithHttpResponse(response: response.response) {
+                                callback(err)
+                                return
+                            }
+                            callback(nil)
+        }
+        
+    }
+    
+}
