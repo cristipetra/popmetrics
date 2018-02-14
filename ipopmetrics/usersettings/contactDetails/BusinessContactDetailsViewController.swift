fb628dd54e4342bdb4675cdb3fdd53f2adcf3d03
Cristian Petra <cristy86_upm@yahoo.com>
2018-02-14 18:32:58 +0200

    Add navigation buttons for nap view

--- ipopmetrics/usersettings/contactDetails/BusinessContactDetailsViewController.swift
+++ ipopmetrics/usersettings/contactDetails/BusinessContactDetailsViewController.swift
@@ -8,36 +8,17 @@
 
 import UIKit
 
-class BusinessContactDetailsViewController: BaseTableViewController {
+class BusinessContactDetailsViewController: SettingsBaseTableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tableView.backgroundColor = PopmetricsColor.tableBackground
-        setUpNavigationBar()
+        
+        setupNavigationBar()
+        self.titleWindow = "CONTACT DETAILS"
     }
-    
-    private func updateView() {
-        //versionLabel.text = UIApplication.versionBuild()
-        //websiteLabel.text = Config.aboutWebLink
-    }
-    
-    private func setUpNavigationBar() {
-        
-        let text = UIBarButtonItem(title: "CONTACT DETAILS", style: .plain, target: self, action: #selector(handlerClickBack))
-        text.tintColor = PopmetricsColor.darkGrey
-        let titleFont = UIFont(name: FontBook.extraBold, size: 18)
-        text.setTitleTextAttributes([NSAttributedStringKey.font: titleFont], for: .normal)
-        text.setTitleTextAttributes([NSAttributedStringKey.font: titleFont], for: .selected)
-        
-        self.navigationController?.navigationBar.backgroundColor = UIColor.white
-        navigationController?.navigationBar.isTranslucent = false
-        
-        let leftButtonItem = UIBarButtonItem.init(image: UIImage(named: "calendarIconLeftArrow"), style: .plain, target: self, action: #selector(handlerClickBack))
-        self.navigationItem.leftBarButtonItems = [leftButtonItem, text]
-        self.navigationItem.leftBarButtonItem?.tintColor = UIColor.black
-        
-    }
+
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let contentView: UIView  = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 60))
@@ -55,10 +36,6 @@ class BusinessContactDetailsViewController: BaseTableViewController {
         }
         
         return contentView
-    }
-    
-    @objc func handlerClickBack() {
-        self.navigationController?.dismissToDirection(direction: .left)
     }
 
 }
