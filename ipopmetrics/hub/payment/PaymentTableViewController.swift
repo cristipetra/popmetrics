076cc05f5c4a078c43039e242d3b0892db49168b
Cristian Petra <cristy86_upm@yahoo.com>
2018-03-01 18:33:25 +0200

    Add payment confirmation view

---
+++ ipopmetrics/hub/payment/PaymentTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentTableViewController.swift
+//  ipopmetrics
+//
+//  Created by Cristian Petra on 01/03/2018.
+//  Copyright © 2018 Popmetrics. All rights reserved.
+//
+
+import UIKit
+
+class PaymentTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
