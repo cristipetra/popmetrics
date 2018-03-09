bae18199ab838bd4d84ce3b98999fc6e7488e9e2
Cristian Petra <cristy86_upm@yahoo.com>
2018-03-09 14:14:03 +0200

    Add cancel action

--- ipopmetrics/helpers/actionsheet/AlertDetails.swift
+++ ipopmetrics/helpers/actionsheet/AlertDetails.swift
@@ -19,13 +19,15 @@ class AlertDetails {
         let actionController = DetailsActionController()
         
         for option in options {
-
             let action = Action(ActionData(title: option.title!, subtitle: option.subtitle!), style: .default, handler: { action in
                 let selectedIndex = options.index(where: {$0.title == action.data?.title && $0.subtitle == action.data?.subtitle})
                 selectedAction!(selectedIndex!)
-            })    
+            })
             actionController.addAction(action)
         }
+        let cancel: Action = Action(ActionData(title: "Cancel"), style: .cancel) { (action) in
+        }
+        actionController.addAction(cancel)
         
         actionController.headerData = "Choose a page to post to"
         parent?.present(actionController, animated: true, completion: nil)
